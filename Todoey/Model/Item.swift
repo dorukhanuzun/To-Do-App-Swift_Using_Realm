89a5bc9ecb9ecb5cf5da3242c3f1eb1e0f636ca5
Dorukhan Uzun <dorukhanuzun@MacBook-Pro.local>
2020-07-17 21:37:16 -0400

    To Do App Using Core Data

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dorukhan Uzun on 2020-07-13.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var checked: Bool = false
+}
